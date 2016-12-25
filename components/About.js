1f76a984e9bc36bd9cd7dc69e850b9bf1cdf92c5
Luke Schoen <ltfschoen@gmail.com>
2016-12-25 16:38:52 +1100

    Added Fluxible boilerplate. Added IDE folders/files to gitignore

---
+++ components/About.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function About() {
+    return (
+        <div>
+            <h2>About</h2>
+            <p>This is a description of the site.</p>
+        </div>
+    );
+};
---
+++ stores/ApplicationStore.js
@@ -0,0 +1,40 @@
+import BaseStore from 'fluxible/addons/BaseStore';
+import RouteStore from './RouteStore';
+
+class ApplicationStore extends BaseStore {
+    constructor(dispatcher) {
+        super(dispatcher);
+        if (this.initialize) {
+            this.initialize();
+        }
+    }
+    initialize() {
+        this.pageTitle = '';
+    }
+    handlePageTitle(currentRoute) {
+        this.dispatcher.waitFor(RouteStore, () => {
+            if (currentRoute && currentRoute.title) {
+                this.pageTitle = currentRoute.title;
+                this.emitChange();
+            }
+        });
+    }
+    getPageTitle() {
+        return this.pageTitle;
+    }
+    dehydrate() {
+        return {
+            pageTitle: this.pageTitle
+        };
+    }
+    rehydrate(state) {
+        this.pageTitle = state.pageTitle;
+    }
+}
+
+ApplicationStore.storeName = 'ApplicationStore';
+ApplicationStore.handlers = {
+    'NAVIGATE_SUCCESS': 'handlePageTitle'
+};
+
+export default ApplicationStore;
